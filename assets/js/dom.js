7ec5295d855701f770c39b7c181b2acc7ad79e56
unknown <mjrjuyel8@gmail.com>
2025-04-04 10:43:34 +0600

    domHandle

---
+++ assets/js/dom.js
@@ -0,0 +1,10 @@
+var element = document.getElementById('demo')
+
+var text = element.innerText;
+console.log(text);
+
+function myName(){
+    let number = Math.ceil(Math.random()*10);
+
+    console.log(number);
+}
