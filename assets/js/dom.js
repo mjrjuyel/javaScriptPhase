f3f769d31c9937a0ef9b2da286f92cdeb681484f
unknown <mjrjuyel8@gmail.com>
2025-04-07 16:49:01 +0600

    Create new element and with this element add some active inactive class and properties

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -18,3 +18,34 @@
 // function image2(){
 //     imageTake.src = '../images/juyel.jpg';
 // }
+
+
+// create new element for a html
+// var h1Header = document.createElement('span');
+// var contentHeader = document.createTextNode('This Is From Span Tag');
+// h1Header.appendChild(contentHeader);
+
+// var content = document.getElementById('li_content');
+// content.appendChild(h1Header);
+
+// h1Header.classList.add('Heading');
+
+var fruits = ['mango','apple','grave','guava'];
+var ulList = document.getElementById('ul_list');
+
+fruits.forEach(fruit => {
+  const li = document.createElement('li');
+  li.textContent = fruit;
+  li.classList.add('active');
+  ulList.appendChild(li);
+});
+document.getElementById('list-container').appendChild(ulList);
+
+var activeClass = document.querySelectorAll('#ul_list li');
+
+activeClass.forEach(item => {
+    item.addEventListener('click', function(){
+        activeClass.forEach(li => {li.innerHTML = 'juyel'});
+        item.innerHTML = "Hello";
+    });
+});
