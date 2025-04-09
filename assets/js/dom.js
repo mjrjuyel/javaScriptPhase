7665592574ffc5c248a6cbe6c9b9076e5c2b7716
unknown <mjrjuyel8@gmail.com>
2025-04-09 23:31:08 +0600

    Created New Element and style these li list by get element with query

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -20,7 +20,8 @@
 // }
 
 
-// create new element for a html
+// create new element for a html  ===========   
+
 // var h1Header = document.createElement('span');
 // var contentHeader = document.createTextNode('This Is From Span Tag');
 // h1Header.appendChild(contentHeader);
@@ -30,22 +31,30 @@
 
 // h1Header.classList.add('Heading');
 
-var fruits = ['mango','apple','grave','guava'];
-var ulList = document.getElementById('ul_list');
+// ====== Create NEw eLEMENT using DOM -=====
+// var fruits = ["Mango","Apple","Orange",'PineApple'];
+// var ulList = document.getElementById('ul_list');
 
-fruits.forEach(fruit => {
-  const li = document.createElement('li');
-  li.textContent = fruit;
-  li.classList.add('active');
-  ulList.appendChild(li);
-});
-document.getElementById('list-container').appendChild(ulList);
+// fruits.forEach(fruit => {
+//     var liList = document.createElement('li');
+//     liList.textContent = fruit;
+//     liList.classList.add('active');
+//     ulList.appendChild(liList);
+// });
 
-var activeClass = document.querySelectorAll('#ul_list li');
 
-activeClass.forEach(item => {
-    item.addEventListener('click', function(){
-        activeClass.forEach(li => {li.innerHTML = 'juyel'});
-        item.innerHTML = "Hello";
-    });
-});
+// var liContent = document.querySelectorAll('#ul_list li');
+
+// liContent.forEach(item => {
+//     item.addEventListener('mouseup', function(e){
+//         liContent.forEach(li => {
+//             li.classList.remove('active');
+//             li.style.fontSize = '10px';
+//         });
+
+//         item.style.fontSize = '20px';
+//     });
+// });
+
+
+
