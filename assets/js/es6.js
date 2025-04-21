7eeaf8cc16b6f5df748aa4566440183d5d913ca6
unknown <mjrjuyel8@gmail.com>
2025-04-21 22:38:30 +0600

    Es6 variales let constl

---
+++ assets/js/es6.js
@@ -0,0 +1,26 @@
+
+// let a = 'Javascript';
+
+// function programming(){
+//   var b = "PHP";
+
+//   console.log(`i love ${a}`);
+//   console.log(`i love ${b}`);
+// }
+// programming()
+// // console.log(`I Know ${b}`);
+// console.log(b);
+
+// if (true) {
+//   let x = 10;
+//   console.log(x); // 10
+// }
+// console.log(x); // 10 (var ব্লক স্কোপ মানে না)
+
+// for(let i= 0; i<10; i++){
+//   console.log(i);
+// }
+
+console.log(a); // undefined (var হোইস্ট হয়)
+let a = 5;
+
